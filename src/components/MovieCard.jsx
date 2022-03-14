00c1bc6f35c1ea4a959d4c1ed492efa719060832
Delin <delinhdln@gmail.com>
2022-03-14 00:49:07 +0000

    reactbasict--

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class MovieCard extends Component {
-  render() {
-    return (
-      <h1>MovieCard</h1>
-    )
-  }
+const MovieCard = () => {
+  return (
+    <div>MovieCard</div>
+  )
 }
 
 export default MovieCard
