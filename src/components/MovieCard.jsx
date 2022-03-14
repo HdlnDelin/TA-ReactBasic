d427be0fcf7e226da1fabad7932237b43feae736
Delin <delinhdln@gmail.com>
2022-03-14 00:40:29 +0000

    react basic-

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class MovieCard extends Component {
+  render() {
+    return (
+      <h1>MovieCard</h1>
+    )
+  }
+}
+
+export default MovieCard
